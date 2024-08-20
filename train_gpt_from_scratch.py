87dbb0d20be3b9d7f7435150413c8067cb27907b
Kammnoel1 <148866344+Kammnoel1@users.noreply.github.com>
2024-08-20 12:49:10 +0200

    corrected code in notebook

--- train_gpt_from_scratch.py
+++ train_gpt_from_scratch.py
@@ -72,28 +72,3 @@ for epoch in range(num_epochs):
     optim.step()
 
     print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item():.4f}")
-
-""" from huggingface_hub import hf_hub_download
-import torch
-from transformers import TimeSeriesTransformerModel
-
-file = hf_hub_download(
-    repo_id="hf-internal-testing/tourism-monthly-batch", filename="train-batch.pt", repo_type="dataset"
-)
-batch = torch.load(file)
-
-model = TimeSeriesTransformerModel.from_pretrained("huggingface/time-series-transformer-tourism-monthly")
-
-# during training, one provides both past and future values
-# as well as possible additional features
-outputs = model(
-    past_values=batch["past_values"],
-    past_time_features=batch["past_time_features"],
-    past_observed_mask=batch["past_observed_mask"],
-    static_categorical_features=batch["static_categorical_features"],
-    static_real_features=batch["static_real_features"],
-    future_values=batch["future_values"],
-    future_time_features=batch["future_time_features"],
-)
-
-last_hidden_state = outputs.last_hidden_state """
